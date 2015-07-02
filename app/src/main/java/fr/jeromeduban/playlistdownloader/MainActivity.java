cfd413f39607b0d1ddc2d93d10cf10b5a139092a
jejefcgb <jduban@enseirb-matmeca.fr>
2015-07-02 23:09:23 +0200

    Recursive calls

--- app/src/main/java/fr/jeromeduban/playlistdownloader/MainActivity.java
+++ app/src/main/java/fr/jeromeduban/playlistdownloader/MainActivity.java
@@ -14,6 +14,7 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import fr.jeromeduban.playlistdownloader.objects.PlayList;
 
@@ -25,6 +26,7 @@ public class MainActivity extends ActionBarActivity {
     private String KEY = "AIzaSyCAsga_OKjW0350A0msLolXm6-B0769unc";
     private String playlistID = "PLTMG0ZyH_DfDs5w40xw2LM0FvMBFtYvqP";
     private OkHttpClient client;
+    private int maxResults = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +34,91 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         client = new OkHttpClient();
-        getPlaylistItems(generateUrl(playlistID, 50));
 
+        ArrayList<PlayList> list = new ArrayList<>();
+        getPlaylistItems(generateUrl(playlistID, maxResults), list);
+
+        // TODO to be used
         String test = "https://www.youtube.com/watch?v=0TFmncOtzcE&index=1&list=PLTMG0ZyH_DfDsK6j40SUmHGgpv7qTa-QA";
-
         String id = test.split("list=")[1].split("&")[0];
         if (id.length() != 34){
             Log.d(TAG,"id might be wrong");
         }
-        Log.d(TAG,id);
+        Log.d(TAG, id);
 
+    }
+    
+    private String generateUrl(String playlistID, int maxResults){
+        return "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults="+Integer.toString(maxResults)+"&playlistId="+playlistID+"&key="+KEY;
+    }
+    private String generateUrl(String playlistID, int maxResults, String nextPageToken){
+        return "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults="+Integer.toString(maxResults)+"&playlistId="+playlistID+"&key="+KEY+"&pageToken="+nextPageToken;
+    }
+
+    private void getPlaylistItems(String url, final ArrayList<PlayList> list){
+
+        System.out.println("URL : "+ url);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+
+            @Override
+            public void onFailure(Request request, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if (!response.isSuccessful())
+                    if (response.code() == 404)
+                        Log.d(TAG, "Error 404");
+                    else
+                        throw new IOException("Unexpected code " + response);
+
+                PlayList pl = parse(response.body().string());
+
+                if (pl != null){
+                    Log.d(TAG, pl.toString());
+                    list.add(pl);
+                    System.out.println("Length : "+ list.size());
+
+                    if (pl.nextPageToken != null){ // If there are another page
+                        System.out.println(Integer.toString(pl.pageInfo.totalResults) + " NOT NULL");
+                        getPlaylistItems(generateUrl(playlistID, maxResults ,pl.nextPageToken), list);
+                    }
+                    else{
+                        System.out.println(Integer.toString(pl.pageInfo.totalResults) + " NULL");
+                        // TODO call merge + treatment
+                    }
+
+                }
+                else
+                    Log.d(TAG, "PL null");
+            }
+        });
+
+
+
+    }
+
+    /**
+     * Parse Json from Youtube API
+     * @param json json from youtube API
+     * @return Playlist as an object
+     */
+    private PlayList parse(String json){
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<PlayList> jsonAdapter = moshi.adapter(PlayList.class);
+        try {
+            return jsonAdapter.fromJson(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
@@ -59,58 +136,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private String generateUrl(String playlistID, int maxResults){
-        return "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults="+Integer.toString(maxResults)+"&playlistId="+playlistID+"&key="+KEY;
-    }
-
-    private void getPlaylistItems(String url){
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        //TODO Add multiple API calls if there is more than 50 songs in the playlist
-        // Try with : https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=18&playlistId=PLTMG0ZyH_DfDs5w40xw2LM0FvMBFtYvqP&key=AIzaSyCAsga_OKjW0350A0msLolXm6-B0769unc
-
-        client.newCall(request).enqueue(new Callback() {
-
-            @Override
-            public void onFailure(Request request, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override public void onResponse(Response response) throws IOException {
-                if (!response.isSuccessful())
-                    if (response.code() == 404)
-                        Log.d(TAG,"Error 404");
-                    else
-                        throw new IOException("Unexpected code " + response);
-
-                PlayList pl = parse(response.body().string());
-
-                if (pl != null)
-                    Log.d(TAG,pl.toString());
-                else
-                    Log.d(TAG,"PL null");
-
-            }
-        });
-    }
-
-    /**
-     * Parse Json from Youtube API
-     * @param json json from youtube API
-     * @return Playlist as an object
-     */
-    private PlayList parse(String json){
-        Moshi moshi = new Moshi.Builder().build();
-        JsonAdapter<PlayList> jsonAdapter = moshi.adapter(PlayList.class);
-        try {
-            return jsonAdapter.fromJson(json);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
